38279666aff55537509592a4bdf5797a4f177d7a
malooney <matthew.looney@icloud.com>
2018-02-27 08:23:58 -0600

    blp code

--- configScript_BLP_Orig.R
+++ configScript_BLP_Orig.R
@@ -4,15 +4,14 @@
 
 rm(list=ls())
 library(hdm)
-data(BLP)
 
 dat <- BLP$BLP
 
 dat$price <- dat$price + 11.761
 
-rm(BLP)
-
 source("/Users/malooney/Google Drive/digitalLibrary/*BLP_Algos/BLP_Algos/BLP_Orig_function.R")
 
-results1 <- BerryLevinsohnPakes(dat=dat, mkt.id.fld = "cdid", prod.id.fld = "id", prc.fld = "price", share.fld="share", x.var.flds = c("hpwt", "air", "mpd", "space"), prc.iv.flds = c("hpwt", "air", "mpg", "space", "trend"), n.sim=200)
+#set.seed(1234)
+
+results_seed3 <- BerryLevinsohnPakes(dat=dat, mkt.id.fld = "cdid", prod.id.fld = "id", prc.fld = "price", share.fld="share", x.var.flds = c("hpwt", "air", "mpd", "space"), prc.iv.flds = c("mpg", "trend"), n.sim=200)
 
